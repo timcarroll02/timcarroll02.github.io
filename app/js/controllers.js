6101405d0284056f6354194a00a5a1cbb7acce67
Colin Carroll <colinc@spiceworks.com>
2014-02-03 23:09:06 -0600

    Got first input box working

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,8 +2,21 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('timWeb.controllers', []).
+  controller('userDetails', ['$scope', function($scope) {
+        $scope.userDetails = {
+            current_principal: 70000,
+            yearly_contributions: 12000,
+            birthday: new Date('1985-11-19'),
+            withdrawal_rate: 0.05,
+            tax_rate: 0.25
+        };
+
+        $scope.numberWithCommas = function(x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+        $scope.interest = function(years){return this.userDetails.current_principal * years;};
 
   }])
   .controller('MyCtrl2', [function() {
